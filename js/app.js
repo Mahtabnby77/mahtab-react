179f0cde601057d6472f7093e11b9e71094e773b
mahtabnabi98 <mahtabnby777@gmail.com>
2023-01-28 17:22:46 +0330

    message

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// js جواب سوال دوم: چهار عمل ریاضی در 
+
+function sum(num1 , num2) {
+    return num1 + num2
+}
+
+function sub(num1 , num2) {
+    return num1 - num2
+}
+
+function mul(num1 , num2) {
+    return num1 * num2
+}
+
+function div(num1 , num2) {
+    return num1 / num2
+}
+
+ console.log(`5 + 12 = ${sum(5 , 12)}`);
+ console.log(`12 - 6 = ${sub(12 , 6)}`);
+ console.log(`7 * 3 = ${mul(7 , 3)}`);
+ console.log(`15 / 3 = ${div(15 , 3)}`);
